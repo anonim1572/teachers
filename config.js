a968e827ea6b03c8ff7c2df6c18b2a0fd1204948
anonim1572 <siema6196@gmail.com>
2025-10-24 18:24:14 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.SUPABASE_CONFIG = {
+  url: "https://kocbevfdxmrdttvrtsep.supabase.co",
+  anonKey:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtvY2JldmZkeG1yZHR0dnJ0c2VwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEyNDk1MjcsImV4cCI6MjA3NjgyNTUyN30.nPGcQWPv6kQX1DzZn3I_KyuyHjbwY4XmbLYR-ismmzE",
+};
